89aa5a86f4d25c114bf43f9151c85eae93823d0a
dragosguta <contact.dragosguta@gmail.com>
2020-05-23 08:06:36 -0700

    release build

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,2 +1,3 @@
 import serverless from './http/serverless';
+export { lambdaOriginRequest } from './http/handler';
 export default serverless;
--- build/index.js
+++ build/index.js
@@ -4,4 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const serverless_1 = __importDefault(require("./http/serverless"));
+var handler_1 = require("./http/handler");
+Object.defineProperty(exports, "lambdaOriginRequest", { enumerable: true, get: function () { return handler_1.lambdaOriginRequest; } });
 exports.default = serverless_1.default;
